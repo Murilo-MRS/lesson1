818c8860e8de35277bdb44deb8545bf1227e5c7d
Murilo-MRS <murilosantana1993@gmail.com>
2022-04-28 15:39:19 -0300

    Preparando app reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+function sum(a,b) {
+  return a + b;
+}
+
+function primeiroJSX() {
+  return(
+    <div>
+      Murilo - Introdução ao ReactJS
+      <h1>Soma: {sum(10, 20)}</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {primeiroJSX()}
     </div>
   );
 }
